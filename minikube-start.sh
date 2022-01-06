fdc5df8c51dcdbaa22de0207520a80e52b96a4e2
birb007 <25516647+birb007@users.noreply.github.com>
2022-01-06 12:48:21 +0000

    [k8s] secure API server with RBAC

--- minikube-start.sh
+++ minikube-start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-minikube start --driver=docker --mount --mount-string $(pwd)/covid_dashboard/:/covid_dashboard
-minikube addons enable ingress
+minikube start \
+    --driver=docker \
+    --mount --mount-string $(pwd)/covid_dashboard/:/covid_dashboard \
+    --extra-config=apiserver.authorization-mode=Node,RBAC || exit $?
+
+minikube addons enable ingress || exit $?
 
 MINIKUBE_IP=$(minikube ip)
 echo $MINIKUBE_IP   covid-africa.info
