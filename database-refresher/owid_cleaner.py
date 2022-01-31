8bd41e4cd1f5afba2150acdf57e418c9157f8453
birb007 <25516647+birb007@users.noreply.github.com>
2022-01-31 18:14:45 +0000

    [CH] add normalised COVID-19 data-points
    Note: the data-points are normalized differently: per million and per
    hundred. Additionally, the new vaccination normalized data-point is
    smoothed which must be acknowledged in data visualisations.

--- database-refresher/owid_cleaner.py
+++ database-refresher/owid_cleaner.py
@@ -2,8 +2,10 @@
 import sys
 import datetime as dt
 
+
 def to_date(string):
     return dt.datetime.strptime(string, "%Y-%m-%d")
+
 
 # A positional argument containing a "YYYY-MM-DD" timestamp
 # represents the most recent batch of data in the database.
@@ -19,6 +21,11 @@ for line in stdin_it:
     cols = line.split(",")
 
     wanted = cols[:4]
+
+    def make_float(index):
+        # convert the value into a float or leave as NULL
+        wanted.append(float(cols[index]) if cols[index] else '')
+
     def make_int(index):
         # convert the value into an integer from a float or leave as NULL
         wanted.append(int(float(cols[index])) if cols[index] else '')
@@ -32,22 +39,30 @@ for line in stdin_it:
         if LAST_UPDATE >= to_date(cols[3]):
             continue
 
-    make_int(4)    # total cases
-    make_int(5)    # new cases
-    make_int(7)    # total deaths
-    make_int(8)    # new deaths
-    make_int(16)   # reproduction rate
-    make_int(25)   # new tests
-    make_int(26)   # total tests
-    make_int(31)   # positive rate
-    make_int(34)   # total vaccinations
-    make_int(35)   # people vaccinated
-    make_int(36)   # people fully vaccinated
-    make_int(37)   # total boosters
-    make_int(38)   # new vaccinations
-    make_int(48)   # population
-    make_int(50)   # median age
-    make_int(53)   # GDP per capita
-    make_int(61)   # life expectancy
-    print(",".join(map(str, wanted)))
+    make_int(4)     # total cases
+    make_int(5)     # new cases
+    make_int(7)     # total deaths
+    make_int(8)     # new deaths
+    make_float(10)  # total cases per million
+    make_float(11)  # new cases per million
+    make_float(14)  # new deaths per million
+    make_float(16)  # reproduction rate
+    make_int(19)    # hospital patients
+    make_float(20)  # hospital patients per million
+    make_int(25)    # new tests
+    make_int(26)    # total tests
+    make_int(31)    # positive rate
+    make_int(34)    # total vaccinations
+    make_int(35)    # people vaccinated
+    make_int(36)    # people fully vaccinated
+    make_int(37)    # total boosters
+    make_int(38)    # new vaccinations
+    make_float(39)  # total boosters per hundred
+    make_float(43)  # people fully vaccinated per hundred
+    make_float(44)  # new vaccinations smoothed per million
+    make_int(48)    # population
+    make_float(50)  # median age
+    make_float(53)  # GDP per capita
+    make_float(61)  # life expectancy
 
+    print(",".join(map(str, wanted)))
