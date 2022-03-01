53244ccda54c0cbeeb548e4deb4ba16588ae6451
birb007 <25516647+birb007@users.noreply.github.com>
2022-03-01 15:20:46 +0000

    [DJ] move utility methods into boilerplate

--- covid_dashboard/tests/test_summary_data_url.py
+++ covid_dashboard/tests/test_summary_data_url.py
@@ -9,27 +9,19 @@ class SummaryDataUrlTestCase(DataUrlBoilerplate, TestCase):
     def test_url_returns_valid_values(self):
         """Check whether the URL returns valid values."""
 
-        def is_optional(value, type):
-            return value is None or isinstance(value, type)
-
-        def ordered(a, b):
-            if a is not b is not None:
-                return a >= b
-            return True
-
         for row in self.content:
             self.assertIsInstance(row[0], str)
-            self.assertTrue(is_optional(row[1], int))
-            self.assertTrue(is_optional(row[2], float))
-            self.assertTrue(is_optional(row[3], int))
-            self.assertTrue(is_optional(row[4], float))
-            self.assertTrue(is_optional(row[5], int))
-            self.assertTrue(is_optional(row[6], float))
+            self.assertTrue(self._is_optional(row[1], int))
+            self.assertTrue(self._is_optional(row[2], float))
+            self.assertTrue(self._is_optional(row[3], int))
+            self.assertTrue(self._is_optional(row[4], float))
+            self.assertTrue(self._is_optional(row[5], int))
+            self.assertTrue(self._is_optional(row[6], float))
 
             # Total instances is less than new instances
-            self.assertTrue(ordered(row[1], row[2]))
-            self.assertTrue(ordered(row[3], row[4]))
-            self.assertTrue(ordered(row[5], row[6]))
+            self.assertTrue(self._is_ordered(row[1], row[2]))
+            self.assertTrue(self._is_ordered(row[3], row[4]))
+            self.assertTrue(self._is_ordered(row[5], row[6]))
 
     def test_url_returns_sorted_locations(self):
         """Check whether the URL returns rows sorted by location."""
