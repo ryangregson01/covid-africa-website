40fae597e1a9b2a9971baaf99e412dbb3f7b5450
Ryan Gregson <2469038g@student.gla.ac.uk>
2022-03-15 16:50:17 +0000

    [DJ] updated tests so they are valid for extra fields in views.
    Some functions in views.py had been refactored, so we must update
    the tests to also check these. Closes #63

--- covid_dashboard/tests/test_summary_data_url.py
+++ covid_dashboard/tests/test_summary_data_url.py
@@ -11,17 +11,21 @@ class SummaryDataUrlTestCase(DataUrlBoilerplate, TestCase):
 
         for row in self.content:
             self.assertIsInstance(row[0], str)
-            self.assertTrue(self._is_optional(row[1], int))
-            self.assertTrue(self._is_optional(row[2], float))
-            self.assertTrue(self._is_optional(row[3], int))
+            self.assertTrue(self._is_optional(row[1], float))
+            self.assertTrue(self._is_optional(row[2], int))
+            self.assertTrue(self._is_optional(row[3], float))
             self.assertTrue(self._is_optional(row[4], float))
             self.assertTrue(self._is_optional(row[5], int))
             self.assertTrue(self._is_optional(row[6], float))
+            self.assertTrue(self._is_optional(row[7], float))
+            self.assertTrue(self._is_optional(row[8], int))
+            self.assertTrue(self._is_optional(row[9], float))
+            self.assertTrue(self._is_optional(row[10], float))
 
-            # Total instances is less than new instances
-            self.assertTrue(self._is_ordered(row[1], row[2]))
+            # Total instances is less than new instances for per 100k values
             self.assertTrue(self._is_ordered(row[3], row[4]))
-            self.assertTrue(self._is_ordered(row[5], row[6]))
+            self.assertTrue(self._is_ordered(row[6], row[7]))
+            self.assertTrue(self._is_ordered(row[9], row[10]))
 
     def test_url_returns_sorted_locations(self):
         """Check whether the URL returns rows sorted by location."""
--- covid_dashboard/tests/test_weekly_maxima_url.py
+++ covid_dashboard/tests/test_weekly_maxima_url.py
@@ -5,27 +5,24 @@ from django.test import TestCase
 
 class WeeklyMaximaUrlTestCase(DataUrlBoilerplate, TestCase):
     ENDPOINT = "get_weekly_maxs"
-    N_TOP = 5
-
-    @staticmethod
-    def _chunk(size, sliceable):
-        for offset in range(0, len(sliceable), size):
-            yield sliceable[offset:offset+size]
 
     def test_url_returns_valid_values(self):
         """Check whether the URL returns valid values."""
 
-        for chunk in self._chunk(self.N_TOP, self.content):
-            for location, value in chunk:
-                self.assertIsInstance(location, str)
-                self.assertIsInstance(value, float)
+        # Each chunk is a different top 5
+        for chunk in self.content:
+            for country in chunk:
+                # location
+                self.assertIsInstance(country[0], str)
+                # value
+                self.assertIsInstance(country[1], float)
 
     def test_url_returns_sorted_values(self):
-        """Check whether the URL returns rows sorted by week."""
+        """Check whether the URL returns rows sorted by size of value."""
 
-        for chunk in self._chunk(self.N_TOP, self.content):
+        for chunk in self.content:
             previous = None
-            for location, value in chunk:
+            for country in chunk:
                 if previous is not None:
-                    self.assertTrue(previous >= value)
-                previous = value
+                    self.assertTrue(previous >= country[1])
+                previous = country[1]
